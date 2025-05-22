e34242860ade4142c26b81815bd2b0cb70b0346d
TrangPham93 <hatrangc2@gmail.com>
2025-05-22 17:12:10 +0300

    Add NULL to args list for redirection

--- myscript.sh
+++ myscript.sh
@@ -33,3 +33,46 @@ cmd path /usr/bin/echo
 shell builtin commandha
 [?2004hminishell$ [?2004l
 exit
+dup2 not failedredirect append work 3
+cmd path /usr/bin/echo
+shell builtin commandkaldk
+[?2004hminishell$ ^C[?2004l
+minishell$ [?2004h[?2004l
+exit
+dup2 not failedredirect append work 3
+cmd path /usr/bin/echo
+check before dup2 not faileddup2 not failedredirect append work 3
+cmd path /usr/bin/echo
+check before checking fd opencheck before dup2 not faileddup2 not failedredirect append work 3
+cmd path /usr/bin/echo
+check before checking fd opencheck before dup2 not faileddup2 not failedredirect append work 3
+cmd path /usr/bin/echo
+check before checking fd opencheck before dup2 not faileddup2 not failedredirect append work 3
+cmd path /usr/bin/echo
+check before checking fd opencheck before dup2 not faileddup2 not failedredirect append work 3
+cmd path /usr/bin/echo
+shell builtin commandhakf
+[?2004hminishell$ ^C[?2004l
+minishell$ [?2004h[?2004l
+exit
+check before checking fd opencheck before dup2 not faileddup2 not failedredirect append work 3
+cmd path /usr/bin/echo
+shell builtin commanddlfakjf
+[?2004hminishell$ ^C[?2004l
+minishell$ [?2004h[?2004l
+exit
+check before checking fd opencheck before dup2 not faileddup2 not failedredirect append work 3
+cmd path /usr/bin/echo
+shell builtin commandsjfak
+[?2004hminishell$ [?2004l
+exit
+check before checking fd opencheck before dup2 not faileddup2 not failedredirect append work 3
+cmd path /usr/bin/echo
+shell builtin commandlsajf
+[?2004hminishell$ [?2004l
+exit
+check before checking fd opencheck before dup2 not faileddup2 not failedredirect append work 3
+cmd path /usr/bin/echo
+shell builtin commandhoa
+[?2004hminishell$ [?2004l
+exit
