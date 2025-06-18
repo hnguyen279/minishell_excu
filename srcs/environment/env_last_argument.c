29f3ff79f167b136251aae517afcb885f8e94e75
Hnguyen <vitcon.kore@gmail.com>
2025-06-18 21:18:36 +0300

    fix .

--- srcs/environment/env_last_argument.c
+++ srcs/environment/env_last_argument.c
@@ -6,7 +6,7 @@
 /*   By: thi-huon <thi-huon@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/08 16:04:47 by thi-huon          #+#    #+#             */
-/*   Updated: 2025/06/18 20:47:55 by thi-huon         ###   ########.fr       */
+/*   Updated: 2025/06/18 21:15:31 by thi-huon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ int	env_set_last_argument(t_shell *mshell, char **cmd)
 	if (i == 0)
 		return (0);
 	last_arg = cmd[i - 1];
-	if (!last_arg[0] || (ft_strcmp(last_arg, "<") == 0)
+	if ((ft_strcmp(last_arg, "<") == 0)
 		|| (ft_strcmp(last_arg, ">") == 0) || (ft_strcmp(last_arg, "<<") == 0)
 		|| (ft_strcmp(last_arg, ">>") == 0)
 		|| (mshell->token_list->type == PIPE))
